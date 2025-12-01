9524992c95491bb87a2c588b1b8930ea493a742f
Chunghyo Ronan Park <chunghyo@troe.kr>
2025-12-02 01:14:10 +0900

    style: Footer 텍스트 가운데 정렬로 변경

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,8 @@ export default function Footer() {
   return (
     <footer className="border-t border-gray-200 bg-white dark:border-gray-800 dark:bg-gray-900">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
-          <div className="text-center sm:text-left">
+        <div className="flex items-center justify-center">
+          <div className="text-center">
             <p className="text-sm text-gray-600 dark:text-gray-400">
               © {currentYear} chunghyo park. Built to move the market. All right reserved. |{' '}
               <a
